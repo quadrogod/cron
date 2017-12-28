a917edeb2dc7423fcbc3e4b0ee1279f15a70fb58
A-lex <A-lex@lenovo>
2017-12-28 21:43:52 +0600

    auto Task identify

--- src/CronManager.php
+++ src/CronManager.php
@@ -12,17 +12,22 @@ abstract class CronManager {
      * @return \Quadrogod\Cron\Task
      * @throws \Exception
      */
-    static function factory($task, $params = null) 
+    static function factory($task = null, $params = null) 
     {
-        $class_name = '\App\Cron\\Task_' . ucfirst($task);
-        //
-        if ( ! class_exists($class_name)) {
-            throw new \Exception('Class \'' . $class_name . '\' not found.');
-        }
-        
+        // get Params
         if ( ($params === null) OR ! is_array($params) ) {
             $params = static::getParams();
         }
+        // get Task Name OR Default
+        if ( ($task === null) OR ! is_string($task) ) {
+            $task = (empty($params['task'])) ? 'Default' : $params['task'];
+        }
+        
+        $class_name = '\App\Cron\\Task_' . ucfirst($task);
+        //
+        if ( ! class_exists($class_name) ) {
+            throw new \Exception('Class \'' . $class_name . '\' not found.');
+        }                
         
         return new $class_name($params);        
     }        
